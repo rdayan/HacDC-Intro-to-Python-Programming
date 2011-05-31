a1eeee9c43d290718c3f33fe72d49771ee75e262
Russ Dayan <rdayan@gmail.com>
2011-05-31 06:26:30 -0400

    Improved animation

--- Dayan.py
+++ Dayan.py
@@ -12,9 +12,11 @@ pygame.display.set_caption("HacDC Intro to Python Programming Homework 1 by Russ
 pygame.draw.rect(screen, (255, 255, 255), (0, 0, 800, 500))
 
 # Define star location variables.
-s1x = 50
-s2y = 225
-s3x = 750
+s1x = 0
+s2y = 175
+s3x = 800
+b1y = 245
+b2x = 0
 
 # Draw animated stars using two triangles.
 while (s1x<200):
@@ -25,8 +27,10 @@ while (s1x<200):
     
     # Advance location variables.
     s1x += 10
-    s2y -= 10
+    s2y -= 5
     s3x -= 10
+    b1y -= 5
+    b2x += 20
     
     # Redraw stars.
     pygame.draw.polygon(screen, (255, 0, 0), [(s1x, 25), (s1x+50, 100), (s1x-50, 100)])
@@ -35,14 +39,17 @@ while (s1x<200):
     pygame.draw.polygon(screen, (255, 0, 0), [(400, s2y+50), (450, s2y-25), (350, s2y-25)])
     pygame.draw.polygon(screen, (255, 0, 0), [(s3x, 25), (s3x+50, 100), (s3x-50, 100)])
     pygame.draw.polygon(screen, (255, 0, 0), [(s3x, 125), (s3x+50, 50), (s3x-50, 50)])
+
+    pygame.draw.rect(screen, (255, 0, 0), (0, b1y, 800, 5))
+    pygame.draw.rect(screen, (255, 0, 0), (400-b2x, 300, b2x*2, 100))
     
     # Redraw display and wait 100 ms.
     pygame.display.flip()
     time.sleep(0.1)    
 
 # Draw flag lines.
-pygame.draw.rect(screen, (255, 0, 0), (0, 150, 800, 100))
-pygame.draw.rect(screen, (255, 0, 0), (0, 300, 800, 100))
+#pygame.draw.rect(screen, (255, 0, 0), (0, 150, 800, 100))
+#pygame.draw.rect(screen, (255, 0, 0), (0, 300, 800, 100))
 
 # Redraw display.
 pygame.display.flip()
